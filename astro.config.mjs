5983b4f125781d88e33438583b8c371d35f1a720
David7ce <david7ce@outlook.com>
2025-06-06 23:55:53 +0100

    fix: correct site URL format in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://david7ce.github.io',
+  site: 'https://david7ce.github.io/',
   outDir: './dist',
   // base: 'name-repository', // Don’t set a base parameter if: pagee on root folder / repository is <USERNAME>.github.io.
   trailingSlash: 'never',
